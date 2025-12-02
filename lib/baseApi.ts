69e5f8130a1cfc4771a8ec0ced5f2754ca41e3c5
mdbayzid2025 <ssmdbayzid23@gmail.com>
2025-12-02 09:35:03 +0600

    uurl

--- lib/baseApi.ts
+++ lib/baseApi.ts
@@ -1,9 +1,9 @@
 import { fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-export const baseUrl = 'http://10.10.7.41:5000/api/v1';
-export const imageUrl = 'http://10.10.7.41:5000';
-// export const baseUrl = 'https://api.mamacaresitters.com/api/v1';
-// export const imageUrl = 'https://api.mamacaresitters.com';
+// export const baseUrl = 'http://10.10.7.41:5000/api/v1';
+// export const imageUrl = 'http://10.10.7.41:5000';
+export const baseUrl = 'https://api.mamacaresitters.com/api/v1';
+export const imageUrl = 'https://api.mamacaresitters.com';
 
 // Centralized base query with automatic token authorization
 export const baseQueryWithAuth = fetchBaseQuery({
